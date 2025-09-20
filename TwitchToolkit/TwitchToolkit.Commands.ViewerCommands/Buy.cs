ec3e3ca2ca1cde046fec751cee451db008d2119c
ekudram <ekudram@hotmail.com>
2025-09-20 16:38:23 -0600

    /*  * File: Buy.cs  * Project: TwitchToolkit  *  * Updated: September 20, 2025  *  * Summary of Changes:  * 1. Changed warning log to debug log for "reached the override" message  * 2. Added null checking for viewer  * 3. Added input validation for command format  * 4. Improved error handling  * 5. Added more descriptive debug logging  */

--- TwitchToolkit/TwitchToolkit.Commands.ViewerCommands/Buy.cs
+++ TwitchToolkit/TwitchToolkit.Commands.ViewerCommands/Buy.cs
@@ -1,15 +1,19 @@
 /*
- * File: ToggleCoins.cs
+ * File: Buy.cs
  * Project: TwitchToolkit
  * 
  * Updated: September 20, 2025
  * 
  * Summary of Changes:
- * 1, 
- * 
+ * 1. Changed warning log to debug log for "reached the override" message
+ * 2. Added null checking for viewer
+ * 3. Added input validation for command format
+ * 4. Improved error handling
+ * 5. Added more descriptive debug logging
  */
 
 using System.Linq;
+using ToolkitCore;
 using TwitchLib.Client.Models;
 using TwitchToolkit.Store;
 using Verse;
@@ -18,13 +22,32 @@ namespace TwitchToolkit.Commands.ViewerCommands;
 
 public class Buy : CommandDriver
 {
-	public override void RunCommand(ChatMessage ChatMessage)
-	{
-		ToolkitLogger.Warning("reached the override");
-		Viewer viewer = Viewers.GetViewer(ChatMessage.Username);
-		if (ChatMessage.Message.Split(' ').Count() >= 2)
-		{
-			Purchase_Handler.ResolvePurchase(viewer, ChatMessage);
-		}
-	}
+    public override void RunCommand(ChatMessage chatMessage)
+    {
+        ToolkitLogger.Debug("Buy command received and processing");
+
+        Viewer viewer = Viewers.GetViewer(chatMessage.Username);
+        if (viewer == null)
+        {
+            ToolkitLogger.Warning($"Could not find viewer for username: {chatMessage.Username}");
+            return;
+        }
+
+        if (viewer.IsBanned)
+        {
+            ToolkitLogger.Debug($"Viewer {chatMessage.Username} is banned, skipping purchase");
+            return;
+        }
+
+        string[] commandParts = chatMessage.Message.Split(' ');
+        if (commandParts.Length < 2)
+        {
+            ToolkitLogger.Debug($"Invalid buy command format from {chatMessage.Username}: {chatMessage.Message}");
+            TwitchWrapper.SendChatMessage($"@{chatMessage.Username} Usage: !buy <item>");
+            return;
+        }
+
+        ToolkitLogger.Debug($"Processing purchase request from {chatMessage.Username} for: {commandParts[1]}");
+        Purchase_Handler.ResolvePurchase(viewer, chatMessage);
+    }
 }
