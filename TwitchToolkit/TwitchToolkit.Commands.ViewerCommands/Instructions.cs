c2598f8e4ba044096966d54496b28c4b7400cf64
ekudram <ekudram@hotmail.com>
2025-09-20 19:39:37 -0600

    Changed to allow for TwitchCore to give friendly message to first time Chatters using !instruction command Mesagehelpers.cs creates the message.

--- TwitchToolkit/TwitchToolkit.Commands.ViewerCommands/Instructions.cs
+++ TwitchToolkit/TwitchToolkit.Commands.ViewerCommands/Instructions.cs
@@ -1,14 +1,24 @@
+/*
+ * Project: Twitch Toolkit
+ * File: Instructions.cs
+ * 
+ * Usage: Implements the !instructions command to provide viewers with information about the mod and available commands.
+ * 
+ */
+
 using ToolkitCore;
-using TwitchLib.Client.Models.Interfaces;
+using TwitchLib.Client.Models;
+using TwitchToolkit.TwitchToolkit.Utilities;
 using Verse;
 
 namespace TwitchToolkit.Commands.ViewerCommands;
 
 public class Instructions : CommandDriver
 {
-	public override void RunCommand(ITwitchMessage twitchMessage)
-	{
-		Command allCommandsCommand = DefDatabase<Command>.GetNamed("AvailableCommands", true);
-		TwitchWrapper.SendChatMessage("@" + twitchMessage.Username + " the toolkit is a mod where you earn coins while you watch. Check out the bit.ly/toolkit-guide  or use !" + allCommandsCommand.command + " for a short list. " + GenText.CapitalizeFirst(ToolkitSettings.Channel) + " has a list of items/events to purchase at " + ToolkitSettings.CustomPricingSheetLink);
-	}
+   
+        public override void RunCommand(ChatMessage ChatMessage)
+        {
+            TwitchWrapper.SendChatMessage(TwitchToolkit.MessageHelpers.GetInstructionsMessage(ChatMessage.Username));
+        }
+   
 }
---
+++ TwitchToolkit/TwitchToolkit.Utilities/MessageHelpers.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Project: TwitchToolkit
+ * File: MessageHelpers.cs
+ * 
+ * Created: September 20, 2025
+ * 
+ * Purpose: Helper class for generating standardized messages
+ * 
+ */
+
+using Verse;
+
+namespace TwitchToolkit.TwitchToolkit.Utilities
+{
+    internal class MessageHelpers
+    {
+        public static string GetInstructionsMessage(string username)
+        {
+            Command allCommandsCommand = DefDatabase<Command>.GetNamed("AvailableCommands", true);
+            return $"@{username} the toolkit is a mod where you earn coins while you watch. Check out the bit.ly/toolkit-guide or use !{allCommandsCommand.command} for a short list. {GenText.CapitalizeFirst(ToolkitSettings.Channel)} has a list of items/events to purchase at {ToolkitSettings.CustomPricingSheetLink}";
+        }
+    }
+}
