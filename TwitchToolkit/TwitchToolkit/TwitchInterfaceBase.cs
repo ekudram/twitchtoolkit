60fc7dc644a5e384781fbd6c4014f0ea71a12615
ekudram <ekudram@hotmail.com>
2025-09-23 10:54:19 -0600

    Added TwitchinfaceBase with Inheritace to help with using TwitchMessageWrapper.

---
+++ TwitchToolkit/TwitchToolkit/TwitchInterfaceBase.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * File: TwitchInterfaceBase.cs
+ * Project: ToolkitCore
+ * 
+ * Updated: October 26, 2023
+ * Modified Using: DeepSeek AI
+ * 
+ * Summary of Changes:
+ * 1. Removed incorrect constructor that tried to pass Game parameter to base.
+ * 2. Added default constructor required for GameComponent.
+ * 
+ * Why These Changes Were Made:
+ * GameComponent in RimWorld does not have a constructor that accepts parameters.
+ * The base GameComponent class only has a default constructor, so we must use that.
+ */
+
+using ToolkitCore;
+using TwitchLib.Client.Models;
+
+namespace TwitchToolkit
+{
+    public abstract class TwitchToolkitInterfaceBase : ToolkitCore.TwitchInterfaceBase
+    {
+        // Add the new method that takes TwitchMessageWrapper
+        public abstract void ParseMessage(TwitchMessageWrapper messageWrapper);
+
+        // Override the existing methods to route to the new method
+        public override void ParseMessage(ChatMessage chatMessage)
+        {
+            ParseMessage(new TwitchMessageWrapper(chatMessage));
+        }
+
+        public override void ParseWhisper(WhisperMessage whisperMessage)
+        {
+            ParseMessage(new TwitchMessageWrapper(whisperMessage));
+        }
+    }
+}
+
+//using TwitchLib.Client.Models.Interfaces;
+//using Verse;
+
+//namespace ToolkitCore
+//{
+//    public abstract class TwitchInterfaceBase : GameComponent
+//    {
+//        public abstract void ParseMessage(ITwitchMessage twitchMessage);
+//    }
+//}
