e1d2c74fd192234181c467a6ec539cc6a00bdb12
ekudram <ekudram@hotmail.com>
2025-09-24 13:11:22 -0600

    Version Update

--- TwitchToolkit/Properties/AssemblyInfo.cs
+++ TwitchToolkit/Properties/AssemblyInfo.cs
@@ -1,14 +1,37 @@
-using System.Diagnostics;
 using System.Collections;
-using System.Runtime.CompilerServices;
-using System.Runtime.Versioning;
+using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using TwitchToolkit.Commands.ViewerCommands;
+using static Unity.Burst.Intrinsics.Arm;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
-[assembly: AssemblyTitle("TwitchStories")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Toolkit")]
+[assembly: AssemblyDescription("2.0e Toolkit Mod for Rimworld")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyCopyright("${Copyright ©CaptoLamia LLC  2025 GNU Affero General Public License}")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.0.10.0")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4151f06c-c71c-4785-bf80-bebaad400ffd")]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major: Increased for large, non - backward - compatible rewrites.
+// Minor: Increased for significant, backward - compatible enhancements.
+// Build: Typically represents a recompilation of the same source(e.g., for bug fixes).
+// Revision: Used for quick fixes like security patches; fully interchangeable with the previous version
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("2.0.*")]
