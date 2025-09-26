2d56bf45184dd6a5f205e70f84963a9eca00a58a
ekudram <ekudram@hotmail.com>
2025-09-26 07:46:47 -0600

    messageWrapper

--- TwitchToolkit/TwitchToolkit/CommandsHandler.cs
+++ TwitchToolkit/TwitchToolkit/CommandsHandler.cs
@@ -34,24 +34,24 @@ public static class CommandsHandler
     /// Processes and executes a Twitch chat command if valid
     /// </summary>
     /// <param name="message">The wrapped Twitch message to process</param>
-    public static void CheckCommand(TwitchMessageWrapper message)
+    public static void CheckCommand(TwitchMessageWrapper messageWrapper)
     {
-        ToolkitLogger.Debug($"CheckCommand called for: {message.Username} - {message.Message}");
-        if (message == null)
+        ToolkitLogger.Debug($"CheckCommand called for: {messageWrapper.Username} - {messageWrapper.Message}");
+        if (messageWrapper == null)
         {
             ToolkitLogger.Warning("Received null twitch message in CheckCommand");
             return;
         }
 
-        ToolkitLogger.Debug($"Checking command - {message.Message}");
+        ToolkitLogger.Debug($"Checking command - {messageWrapper.Message}");
 
-        if (string.IsNullOrEmpty(message.Message))
+        if (string.IsNullOrEmpty(messageWrapper.Message))
         {
             ToolkitLogger.Debug("Message is null or empty");
             return;
         }
 
-        string user = message.Username;
+        string user = messageWrapper.Username;
         Viewer viewer = Viewers.GetViewer(user);
 
         if (viewer == null)
@@ -72,25 +72,25 @@ public static class CommandsHandler
 
         // Find the command definition that matches the message
         Command commandDef = DefDatabase<Command>.AllDefs
-            .FirstOrDefault(s => message.Message.StartsWith("!" + s.command));
+            .FirstOrDefault(s => messageWrapper.Message.StartsWith("!" + s.command));
 
         if (commandDef == null)
         {
-            ToolkitLogger.Debug($"No command definition found for message: {message.Message}");
+            ToolkitLogger.Debug($"No command definition found for message: {messageWrapper.Message}");
             return;
         }
 
-        string permissionError = ValidateCommandPermissions(commandDef, viewer, message);
+        string permissionError = ValidateCommandPermissions(commandDef, viewer, messageWrapper);
         if (!string.IsNullOrEmpty(permissionError))
         {
             SendFeedback(permissionError);
             return;
         }
 
-        ExecuteCommand(commandDef, message);
+        ExecuteCommand(commandDef, messageWrapper);
     }
 
-    private static string ValidateCommandPermissions(Command commandDef, Viewer viewer, TwitchMessageWrapper message)
+    private static string ValidateCommandPermissions(Command commandDef, Viewer viewer, TwitchMessageWrapper messageWrapper)
     {
         if (!commandDef.enabled)
         {
@@ -98,7 +98,7 @@ public static class CommandsHandler
             return $"Sorry {viewer.username}, that command is currently disabled.";
         }
 
-        if (commandDef.requiresMod && !viewer.mod && !message.IsModerator && !IsChannelOwner(viewer))
+        if (commandDef.requiresMod && !viewer.mod && !messageWrapper.IsModerator && !IsChannelOwner(viewer))
         {
             ToolkitLogger.Debug($"Viewer {viewer.username} lacks mod privileges for command '{commandDef.command}'");
             return $"Sorry {viewer.username}, you need to be a moderator to use that command.";
@@ -118,17 +118,17 @@ public static class CommandsHandler
         return viewer.username.Equals(ToolkitSettings.Channel, StringComparison.OrdinalIgnoreCase);
     }
 
-    private static void ExecuteCommand(Command commandDef, TwitchMessageWrapper message)
+    private static void ExecuteCommand(Command commandDef, TwitchMessageWrapper messageWrapper)
     {
         try
         {
-            ToolkitLogger.Log($"Executing command '{commandDef.command}' for viewer '{message.Username}'");
-            commandDef.RunCommand(message);
+            ToolkitLogger.Log($"Executing command '{commandDef.command}' for viewer '{messageWrapper.Username}'");
+            commandDef.RunCommand(messageWrapper);
         }
         catch (Exception ex)
         {
             ToolkitLogger.Error($"Error executing command '{commandDef.command}': {ex}");
-            SendFeedback($"Sorry {message.Username}, there was an error executing your command.");
+            SendFeedback($"Sorry {messageWrapper.Username}, there was an error executing your command.");
         }
     }
 
